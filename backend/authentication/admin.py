78ab25d2e55d9a6533075b74dd6acff35b274b91
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-12-06 09:34:16 -0800

    Admin changes

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -3,7 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class CustomUserAdmin(UserAdmin):
-    pass
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ("is_Pharmacist", "is_Technician",)}),
+    )
 
 # Register your models here.
 admin.site.register(User, CustomUserAdmin)
