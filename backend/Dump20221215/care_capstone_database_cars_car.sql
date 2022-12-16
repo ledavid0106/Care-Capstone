7a55e8cfa9f89dc33f86a8f23774d61c2704e711
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-12-15 17:31:34 -0800

    mysql

--- backend/Dump20221215/care_capstone_database_cars_car.sql
+++ backend/Dump20221215/care_capstone_database_cars_car.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `care_capstone_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `care_capstone_database`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: care_capstone_database
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-15 17:17:12
+-- Dump completed on 2022-12-15 17:31:11
